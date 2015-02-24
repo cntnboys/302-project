4b7589d989e76e705da5c7b304ff40b41eeea72d
IrisAmp - Laptop <brandon.d.yue@gmail.com>
2015-02-24 10:04:47 -0700

    More placeholders & organization

---
+++ Android/app/src/main/java/ca/ualberta/medroad/model/Dummy.java
@@ -0,0 +1,8 @@
+package ca.ualberta.medroad.model;
+
+/**
+ * Created by Yuey on 2015-02-24.
+ */
+public class Dummy
+{
+}
---
+++ Android/app/src/main/java/ca/ualberta/medroad/view/PlaceholderFragment.java
@@ -0,0 +1,49 @@
+package ca.ualberta.medroad.view;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import ca.ualberta.medroad.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link PlaceholderFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class PlaceholderFragment
+		extends Fragment
+{
+	public static PlaceholderFragment newInstance( )
+	{
+		PlaceholderFragment fragment = new PlaceholderFragment();
+		Bundle args = new Bundle();
+		fragment.setArguments( args );
+		return fragment;
+	}
+
+	public PlaceholderFragment()
+	{
+		// Required empty public constructor
+	}
+
+	@Override
+	public void onCreate( Bundle savedInstanceState )
+	{
+		super.onCreate( savedInstanceState );
+	}
+
+	@Override
+	public View onCreateView( LayoutInflater inflater,
+							  ViewGroup container,
+							  Bundle savedInstanceState )
+	{
+		return inflater.inflate( R.layout.placeholder, container, false );
+	}
+
+
+}
