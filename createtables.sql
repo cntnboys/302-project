7217ef2585281c180ae40abd34347b2e7c48b897
jdn5 <jdn5@ualberta.ca>
2015-03-05 14:05:22 -0700

    Edited table statements

--- createtables.sql
+++ createtables.sql
@@ -3,16 +3,24 @@ create table patient(
 	lastname	varchar(10),
 	firstname	varchar(10),
 	healthno	char(9),
-	primary key (sin)
+	dob		date,
+	gender	char(1),
+	primary key (sin),
+	
 )
 
 create table session(
-	ecg		float,
+	ecgpeak	float,
+	interval	float,
+	sample	float,
+	systolic	float,
+	dystolic	float,
+	map		float,
 	o2		float,
 	bloodo2	float,
-	sestime		time,
-	sesdate		date,
-	primary key (time)
+	timestamp	time,
+	datestamp	date,
+	primary key (timestamp)
 )
 create table account(
 	username	varchar(10),
