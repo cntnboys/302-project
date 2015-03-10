de614ab73d0ebdc97e29fb19863000eedc8645aa
jdn5 <jdn5@ualberta.ca>
2015-03-10 14:39:22 -0600

    made major changes to the sql tables to meet new specifications

--- createtables.sql
+++ createtables.sql
@@ -1,14 +1,41 @@
 create table patient(
-	sin		char(9),
-	lastname	varchar(10),
-	firstname	varchar(10),
-	healthno	char(9),
+	name		varchar(10),
+	ahcn		char(9),
 	dob		date,
-	gender	char(1),
-	primary key (sin),
-	
+	primary key (healthno)
 )
+create table ecg(
+	ahcn		char(9),
+	mv		int,
+	pulse		int,
+	o2		int,
+	diabp		int,
+	sysbp		int,
+	map		int,
+	timestamp	time,
+	ses_no	int,
+	primary key (ahcn)
+)
+create table oximiter(
+	ahcn		char(9),
+	pulse		int,
+	o2		int,
+	timestamp	time,
+	ses_no	int,
+	primary key (ahcn)
+)
+create table bpressure(
+	ahcn		char(9),
+	diabp		int,
+	sysbp		int,
+	map		int,
+	timestamp	time,
+	ses_no	int,
+	primary key (ahcn)
+)
+(The tables during planning had variables 'type	ecg', 'type	o2', 'type	bpressure'	on the bottom of the 3 reading tables.  I am not sure if these are relevant to the tables or database.  Notify if they are important to add).
 
+(Unsure if session is relevant or not.  Feel free to ignore or delete it if necessary.)
 create table session(
 	ecgpeak	float,
 	interval	float,
