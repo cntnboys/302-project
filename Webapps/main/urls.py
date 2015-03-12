83daa8509141c229a811ae91e068f5de17e20e6e
cntnboys <cpalexan@ualberta.ca>
2015-03-12 14:01:42 -0600

    views

--- Webapps/main/urls.py
+++ Webapps/main/urls.py
@@ -14,16 +14,10 @@ urlpatterns = patterns('',
                        
     url(r'^login/$', views.loginPage, name='LoginPage'),
     url(r'^logout/$', views.logout, name="logout"),
-    url(r'^register/$', views.registerPage, name='RegisterPage'),
-    url(r'^(?P<current_user>.+?)/posts/', views.mainPage, name='mainPage'),
+    
     
    
-    url(r'^(?P<theusername>.+?)/(?P<user_id>.+?)/$', views.getaProfile, name='getaProfile'),
-    url(r'^(?P<current_user>.+?)/(?P<current_userid>.+?)/edit', views.editProfile, name='EditProfile'),
-    url(r'^makePost/$', views.makePost, name='makePost'),
-    url(r'^searchPage/$', views.searchPage, name='SearchPage'),
-    url(r'^friends/$',views.friends, name='userFriends'),
-    url(r'^friendRequest/$', views.friendRequest, name='friendRequest'),
+   
 )
 
 if settings.DEBUG:
