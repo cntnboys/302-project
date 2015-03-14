891c41b13d1df710362970b67362db4ef02f81ec
chunhanlee <chunhan@ualberta.ca>
2015-03-14 17:20:26 -0600

    model change

--- Webapps/main/migrations/0001_initial.py
+++ Webapps/main/migrations/0001_initial.py
@@ -11,38 +11,37 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
+            name='ECG',
+            fields=[
+                ('patient_id', models.IntegerField(serialize=False, primary_key=True)),
+                ('mv', models.IntegerField()),
+                ('pulse', models.IntegerField()),
+                ('oxygen', models.IntegerField()),
+                ('diastolicbp', models.IntegerField()),
+                ('systolicbp', models.IntegerField()),
+                ('map2', models.IntegerField()),
+                ('timestamp', models.DateTimeField(auto_now_add=True)),
+                ('session_id', models.IntegerField()),
+            ],
+            options={
+                'db_table': 'ecg',
+                'verbose_name': 'ECG',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
             name='Patient',
             fields=[
                 ('patient_id', models.IntegerField(serialize=False, primary_key=True)),
                 ('ahcn', models.CharField(max_length=9, unique=True, null=True)),
                 ('dob', models.DateField()),
-                ('liveStatus', models.BooleanField(default=False)),
+                ('liveStatus', models.CharField(default=b'n', max_length=1)),
                 ('doctor', models.CharField(max_length=5, null=True)),
                 ('name', models.CharField(max_length=10, null=True)),
             ],
             options={
                 'db_table': 'patients',
                 'verbose_name': 'Patient',
-            },
-            bases=(models.Model,),
-        ),
-        migrations.CreateModel(
-            name='ECG',
-            fields=[
-                ('patient_id', models.ForeignKey(primary_key=True, db_column=b'patient_id', serialize=False, to='main.Patient')),
-                ('mv', models.IntegerField()),
-                ('pulse', models.IntegerField()),
-                ('oxygen', models.IntegerField()),
-                ('diastolicbp', models.IntegerField()),
-                ('systolicbp', models.IntegerField()),
-                ('map', models.IntegerField()),
-                ('timestamp', models.DateTimeField(auto_now_add=True)),
-                ('session_id', models.IntegerField(unique=True)),
-                ('deviceType', models.CharField(max_length=10, null=True)),
-            ],
-            options={
-                'db_table': 'ecg',
-                'verbose_name': 'ECG',
             },
             bases=(models.Model,),
         ),
