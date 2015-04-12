39c2063db10a31d6eecce914b063bc3f566d4348
dbsigurd <dbsigurd@ualberta.ca>
2015-04-12 12:00:21 -0600

    for the love of god please work

--- Webapps/main/static/js/panelScript.js
+++ Webapps/main/static/js/panelScript.js
@@ -25,27 +25,22 @@ var start = new Date().getTime();
 var j=0;
 var i =0;
 var start = new Date().getTime();
-function httpGet() {
-    try {
-    var xmlHttp= new XMLHttpRequest();
-    var ptid=document.getElementById('pId');
-    var ptString=1;
-    var pageString = "https://project302.herokuapp.com/main/getPatientdata/";
-    var slash ="/";
-    var page= pageString.concat(ptString);
-    var pageFinal= page.concat(slash);
-
-    xmlHttp.open("GET",pageFinal,false);
-    xmlHttp.send(null);
-  }catch{
-
-  }
-  //  var dataString = "{'ecgobject': [[<ECG: patient_id: 1 mv: 12 pulse: 12 oxygen: 12 diastolicbp: 12systolicbp: 12map: 12timestamp: 2015-03-15 04:42:38.315322+00:00session_id: 123>]]}"
-    //window.alert(xmlHttp.responseText);
-    //window.alert(xmlHttp.responseText);
-    var dataString= xmlHttp.responseText;
-    stringToData(dataString);
-}
+// function httpGet() {
+//
+//     var xmlHttp= new XMLHttpRequest();
+//     var ptid=document.getElementById('pId');
+//     var ptString=1;
+//     var pageString = "https://project302.herokuapp.com/main/getPatientdata/";
+//     var slash ="/";
+//     var page= pageString.concat(ptString);
+//     var pageFinal= page.concat(slash);
+//
+//     xmlHttp.open("GET",pageFinal,false);
+//     xmlHttp.send(null);
+//
+//     var dataString= xmlHttp.responseText;
+//     stringToData(dataString);
+// }
 var pulseRat,diaLoc,sysLoc,mapLoc,oxySub,diaCal,sysCal;
 
 function stringToData(requestString){
@@ -73,44 +68,44 @@ function stringToData(requestString){
 function hrAlert(){
     //window.alert("HEART RATE TOO LOW");
 }
-function bploop(){
-    sleep(1000);
-    httpGet();
-    var dia=document.getElementById('dia');
-    var sys=document.getElementById('sys');
-    var oxim=document.getElementById('oxim');
-
-    diaRate=diaCal;
-    sysRate=sysCal;
-    oxRate=oxyCal;
-
-    dia.innerHTML=diaRate;
-    sys.innerHTML=sysRate;
-    oxim.innerHTML=oxRate;
-
-    if(sysRate<100){
-        sys.style.color="red";
-    }else if(sysRate>200){
-        sys.style.color='red';
-    }else{
-        sys.style.color='green';
-    }
-
-    if(diaRate<=110){
-        dia.style.color="green";
-    }else {
-        dia.style.color='red';
-    }
-    if(oxRate<94){
-        oxim.style.color="red";
-    }else{
-        oxim.style.color='green';
-    }
-
-    //setInterval(bploop,1000);
-    requestAnimationFrame(bploop);
-}
-bploop();
+// function bploop(){
+//     sleep(1000);
+//     httpGet();
+//     var dia=document.getElementById('dia');
+//     var sys=document.getElementById('sys');
+//     var oxim=document.getElementById('oxim');
+//
+//     diaRate=diaCal;
+//     sysRate=sysCal;
+//     oxRate=oxyCal;
+//
+//     dia.innerHTML=diaRate;
+//     sys.innerHTML=sysRate;
+//     oxim.innerHTML=oxRate;
+//
+//     if(sysRate<100){
+//         sys.style.color="red";
+//     }else if(sysRate>200){
+//         sys.style.color='red';
+//     }else{
+//         sys.style.color='green';
+//     }
+//
+//     if(diaRate<=110){
+//         dia.style.color="green";
+//     }else {
+//         dia.style.color='red';
+//     }
+//     if(oxRate<94){
+//         oxim.style.color="red";
+//     }else{
+//         oxim.style.color='green';
+//     }
+//
+//     //setInterval(bploop,1000);
+//     requestAnimationFrame(bploop);
+// }
+// bploop();
 function incrementx(){
   if (i==ECGMvList.length){
 
